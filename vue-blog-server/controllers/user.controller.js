7f656719a43d813e54e0e40f6589065f383494a7
MarkTo22 <stone_hzz@163.com>
2017-03-28 18:57:04 +0800

    commit1
    commit1

--- vue-blog-server/controllers/user.controller.js
+++ vue-blog-server/controllers/user.controller.js
@@ -5,5 +5,14 @@ const User = require('../models/user.model');
 exports.create = function(req,res,next){
     const user = new User(req.body);
     // console.log(user);
-    res.json(user);
+    //res.json(user);//postman--显示返回json数据
+    user.save()
+        .then(data => {
+            res.json(data)
+        })
+        // .catch(e => {
+        //     res.status(400)
+        //         .send(message.error400);
+        // })
+    
 };
